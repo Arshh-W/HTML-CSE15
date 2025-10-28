ea1bf16b3af8567217662d05b789d62c40af87f7
Arsh Rana <q43676709@gmail.com>
2025-10-28 16:29:22 +0530

    Add new project files and resolve submodule issue

---
+++ HTML-CSE15/Untitled-1.py
@@ -0,0 +1,4 @@
+#Document editor with undo and redo buttons 
+#multi-documents, containing temporary save button(saved till the instance of .exe is still on
+#dialogue box for pop ups
+#libraries to be used- tkinter( for interface), pyqw
